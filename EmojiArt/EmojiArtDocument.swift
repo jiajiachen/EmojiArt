00c3ccd750408cea64d6aa020360a14dcff5d10c
KellyChen <matijueming@gmail.com>
2024-10-05 13:09:32 +0800

    lecture 10

--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -1,7 +1,6 @@
 //
-//  EmojiArtDocument.swift
-//  EmojiArt
-//
+//  EmojiArtDocumentswift
+//  EmojiArt//
 //  Created by Kelly Chen on 2024/10/4.
 //
 
@@ -10,7 +9,12 @@ import SwiftUI
 class EmojiArtDocument: ObservableObject {
     typealias Emoji = EmojiArt.Emoji
     
-    private var emojiArt = EmojiArt()
+    @Published private var emojiArt = EmojiArt()
+    
+    init() {
+        emojiArt.addEmoji("🚲", at: .init(x: -200, y: -150), size: 200)
+        emojiArt.addEmoji("🔥", at: .init(x: 250, y: 100), size: 80)
+    }
     
     var emojis: [Emoji] {
         emojiArt.emojis
@@ -24,5 +28,23 @@ class EmojiArtDocument: ObservableObject {
     
     func setBackground(_ url: URL?) {
         emojiArt.background = url
+    }
+    
+    func addEmoji(_ emoji: String, at position: Emoji.Position, size: CGFloat) {
+        emojiArt.addEmoji(emoji, at: position, size: Int(size))
+    }
+}
+
+extension EmojiArt.Emoji {
+    var font: Font {
+        Font.system(size: CGFloat(size))
+    }
+}
+
+extension EmojiArt.Emoji.Position {
+    func `in`(_ geometry: GeometryProxy) -> CGPoint {
+        let center = geometry.frame(in: .local).center
+        return CGPoint(x: center.x + CGFloat(x),
+                       y:center.y - CGFloat(y))
     }
 }
--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct EmojiArtDocumentView: View {
+    typealias Emoji = EmojiArt.Emoji
+    
+    @ObservedObject var document: EmojiArtDocument
     
     private let emojis = "👻🍎😃🤪☹️🤯🐶🐭🦁🐵🦆🐝🐢🐄🐖🌲🌴🌵🍄🌞🌎🔥🌈🌧️🌨️☁️⛄️⛳️🚗🚙🚓🚲🛺🏍️🚘✈️🛩️🚀🚁🏰🏠❤️💤⛵️"
     private let paletteEmojiSize: CGFloat = 40
     
     var body: some View {
         VStack(spacing: 0) {
-            Color.yellow
+            documentBody
             ScrollingEmojis(emojis)
                 .font(.system(size: paletteEmojiSize))
                 .padding(.horizontal)
@@ -22,7 +25,55 @@ struct EmojiArtDocumentView: View {
         }
         
     }
+    
+    private var documentBody: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Color.white
+                AsyncImage(url: document.background)
+                    .position(Emoji.Position.zero.in(geometry))
+                ForEach(document.emojis) { emoji in
+                    Text(emoji.string)
+                        .font(emoji.font)
+                        .position(emoji.position.in(geometry))
+                }
+            }
+            .dropDestination(for: Sturldata.self) { sturldatas, location in
+                return drop(sturldatas, at: location, in: geometry)
+            }
+        }
+       
+    }
+    
+    private func drop(_ sturldatas: [Sturldata], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
+        for sturldata in sturldatas {
+            switch sturldata {
+            case .url(let url):
+                document.setBackground(url)
+                return true
+            case .string(let emoji):
+                document.addEmoji(
+                    emoji,
+                    at: emojiPosition(at: location, in: geometry),
+                    size: paletteEmojiSize
+                )
+                return true
+            default:
+                break
+            }
+        }
+        return false
+    }
+    
+    private func emojiPosition(at location: CGPoint, in geometry: GeometryProxy) -> Emoji.Position {
+        let center = geometry.frame(in: .local).center
+        return Emoji.Position(
+            x: Int(location.x - center.x),
+            y: Int(-(location.y - center.y)))
+    }
 }
+
+
 
 struct ScrollingEmojis: View {
     let emojis: [String]
@@ -36,6 +87,7 @@ struct ScrollingEmojis: View {
             HStack {
                 ForEach(emojis, id: \.self) { emoji in
                     Text(emoji)
+                        .draggable(emoji)
                 }
             }
         }
@@ -43,5 +95,5 @@ struct ScrollingEmojis: View {
 }
 
 #Preview {
-    EmojiArtDocumentView()
+    EmojiArtDocumentView(document: EmojiArtDocument())
 }
