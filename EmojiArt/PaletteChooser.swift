c4128eb0764f1a37a9278e66a230213d0697b7e9
KellyChen <matijueming@gmail.com>
2024-10-06 12:50:40 +0800

    lecture 11

--- EmojiArt/PaletteChooser.swift
+++ EmojiArt/PaletteChooser.swift
@@ -8,11 +8,69 @@
 import SwiftUI
 
 struct PaletteChooser: View {
+    @EnvironmentObject var store: PaletteStore
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            chooser
+            view(for: store.palettes[store.cursorIndex])
+        }
+        .clipped()
+    }
+    
+    var chooser: some View {
+//        Button {
+//            withAnimation {
+//                store.cursorIndex += 1
+//            }
+//            
+//        } label: {
+//            Image(systemName: "paintpalette")
+//        }
+        
+        AnimatedActionButton(systemImage: "paintpalette") {
+            store.cursorIndex += 1
+        }
+        .contextMenu {
+            AnimatedActionButton("New", systemImage: "plus") {
+                store.insert(name: "Math", emojis: "➕➖✖️➗")
+            }
+            AnimatedActionButton("Delete", systemImage: "minus.circle", role: .destructive) {
+                store.palettes.remove(at: store.cursorIndex)
+            }
+        }
+    }
+    
+    func view(for palette: Palette) -> some View {
+        HStack {
+            Text(palette.name)
+            ScrollingEmojis(palette.emojis)
+        }
+        .id(palette.id)
+        .transition(.asymmetric(insertion: .move(edge: .bottom), removal: .move(edge: .top)))
+    }
+}
+
+struct ScrollingEmojis: View {
+    let emojis: [String]
+    
+    init(_ emojis: String) {
+        self.emojis = emojis.uniqued.map(String.init)
+    }
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(emojis, id: \.self) { emoji in
+                    Text(emoji)
+                        .draggable(emoji)
+                }
+            }
+        }
     }
 }
 
 #Preview {
     PaletteChooser()
+        .environmentObject(PaletteStore(named: "Preview"))
 }
