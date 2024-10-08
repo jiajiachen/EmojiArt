bcdb025eb82dc685a30bdd2ed8f63d0cd951bc2e
KellyChen <matijueming@gmail.com>
2024-10-08 20:57:29 +0800

    add palettemanager file

---
+++ EmojiArt/PaletteManager.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteManager.swift
+//  EmojiArt
+//
+//  Created by Kelly Chen on 2024/10/8.
+//
+
+import SwiftUI
+
+struct PaletteManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaletteManager()
+}
