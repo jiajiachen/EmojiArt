746418aec254aa9d13f20bf5e62fe863ed8aeb25
KellyChen <matijueming@gmail.com>
2024-10-08 20:12:14 +0800

    navigation

---
+++ EmojiArt/PaletteEditor.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteEditor.swift
+//  EmojiArt
+//
+//  Created by Kelly Chen on 2024/10/7.
+//
+
+import SwiftUI
+
+struct PaletteEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaletteEditor()
+}
---
+++ EmojiArt/PaletteList.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteList.swift
+//  EmojiArt
+//
+//  Created by Kelly Chen on 2024/10/7.
+//
+
+import SwiftUI
+
+struct PaletteList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaletteList()
+}
